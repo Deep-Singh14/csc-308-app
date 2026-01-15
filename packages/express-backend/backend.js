c57781d382d439f5f1d85973b9fc03c44c2f6f5f
Deep-Singh14 <khushdip14@gmail.com>
2026-01-15 12:46:36 -0800

    added deleteUserById

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -11,9 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 const users = {
@@ -21,35 +19,33 @@ const users = {
     {
       id: "xyz789",
       name: "Charlie",
-      job: "Janitor"
+      job: "Janitor",
     },
     {
       id: "abc123",
       name: "Mac",
-      job: "Bouncer"
+      job: "Bouncer",
     },
     {
       id: "ppp222",
       name: "Mac",
-      job: "Professor"
+      job: "Professor",
     },
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspring actress",
     },
     {
       id: "zap555",
       name: "Dennis",
-      job: "Bartender"
-    }
-  ]
+      job: "Bartender",
+    },
+  ],
 };
 
 const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+  return users["users_list"].filter((user) => user["name"] === name);
 };
 
 app.get("/users", (req, res) => {
@@ -85,4 +81,23 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+const deleteUserById = (id) => {
+  const user = users["users_list"].findIndex((user) => user["id"] === id);
+  if (user !== -1) {
+    users["users_list"].splice(user, 1);
+    return true;
+  }
+  return false;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const isDeleted = deleteUserById(id);
+  if (isDeleted) {
+    res.send();
+  } else {
+    res.status(404).send("User not found.");
+  }
 });
